1a1311c4f2996fcfc98ada35ea86d7f1aac761cd
nikhil <nikhil@codeacious.com>
2018-12-03 23:10:56 +0530

    replace routes to controller

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var cors = require('cors')
 
 mongoose.connect('mongodb://localhost/stockpile');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./app/controllers/api/v1/users');
 
 var app = express();
 
@@ -18,14 +17,14 @@ app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
